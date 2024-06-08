1dce86e5d608a5d53bcdf31c3fad561bee384130
karavi <karavi@ntk.ir>
2024-06-08 19:51:20 +0330

    indo

--- src/app/app-stand-alone.component.ts
+++ src/app/app-stand-alone.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TranslateModule],
   templateUrl: './app-stand-alone.component.html',
 })
 export class AppStandAloneComponent {
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,29 @@
-import { ApplicationConfig, isDevMode, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
+import { provideHttpClient } from '@angular/common/http';
+import { ApplicationConfig, importProvidersFrom, isDevMode, provideZoneChangeDetection } from '@angular/core';
+import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideRouter } from '@angular/router';
 import { provideServiceWorker } from '@angular/service-worker';
 import { routes } from './app.routes';
+import { NgxTranslateModule } from './core/i18n/ngxTranslateModule';
 
 export const appConfig: ApplicationConfig = {
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideAnimations(), // required animations providers
+    provideHttpClient(),
+    importProvidersFrom(NgxTranslateModule),
+    provideRouter(routes),
+    provideServiceWorker('ngsw-worker.js',
+      {
+        enabled: !isDevMode(),
+        registrationStrategy: 'registerWhenStable:30000'
+      })
+  ]
+};
+
+
+export const appConfig1: ApplicationConfig = {
 
 
   providers: [
